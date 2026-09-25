94988a8d12b9929e774c44d2f1f856424312cc7b
pacman-Sy <forliga20@gmail.com>
2026-09-25 08:55:03 +0000

    ci: restore kernel assembler alias safely

--- kerdevdep/setup_kerdevdep.sh
+++ kerdevdep/setup_kerdevdep.sh
@@ -159,6 +159,9 @@ fi
 WRAPPERS
 chmod +x bin/ccache
 
+# Clang's -no-integrated-as mode looks for an executable named "as".
+ln -sf llvm-as "$SCRIPT_DIR/clang/bin/as"
+
 # Symlink clang binaries
 for f in clang/bin/*; do
     name="$(basename "$f")"
